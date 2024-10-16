3bd44f2509502e722604de7dac980d232a123cb5
YOS0602 <yoshitaka.t.biz@gmail.com>
2024-10-16 23:38:25 +0900

    🌟 Add logo favicon

--- tomo/src/main.tsx
+++ tomo/src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import App from "./App.tsx";
 import "./index.css";
-import "@fontsource/noto-sans-jp/300.css";
 import "@fontsource/noto-sans-jp/400.css";
-import "@fontsource/noto-sans-jp/500.css";
-import "@fontsource/noto-sans-jp/700.css";
 
 // MUI Component で使用するデフォルトフォントを設定
 const theme = createTheme({
